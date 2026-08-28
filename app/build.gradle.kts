5e8fea51700b27f6392885fd8bd630d51327c8e9
waxew <4622.ar@gmail.com>
2026-08-29 02:50:56 +0330

    Bump App-NameDic to 1.3.1 and keep Android 7 desugaring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "ir.asteam.namedic"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.3.0"
+        versionCode = 8
+        versionName = "1.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
