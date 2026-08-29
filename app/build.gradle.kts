8643e55765ac1a6a429c572cc26ab604e72a23f0
waxew <4622.ar@gmail.com>
2026-08-29 13:04:17 +0330

    Bump App-NameDic to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "ir.asteam.namedic"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.4.0"
+        versionCode = 10
+        versionName = "1.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
